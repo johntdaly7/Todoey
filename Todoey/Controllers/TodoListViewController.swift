576e8a130d4745a8221b0cec3d18b0ce89f25392
John Daly <johntdaly@johns-macbook-pro.local>
2019-01-25 14:25:10 -0500

    committing just to save

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     //var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     var itemArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext //gets access to the AppDelegate as a singleton object
     
     //let defaults = UserDefaults.standard //UserDefaults: an interface to the user's defaults database, where you store key-value pairs persistently across launches of the app. UserDefaults should only be used to save/persist small bits of data (like a volumne value or boolean or player name, should not use arrays often or heavily) (should not be used as a database). Just to get one value from the Defaults, the iPhone/Device has to load ALL of the defaults, instead of just that one value. UserDefaults is a singleton
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist") //the user domain mask is the user's home directory -> the place where we are to save their personal items associated with this current app. We grab the first item because this is an array.
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist") //the user domain mask is the user's home directory -> the place where we are to save their personal items associated with this current app. We grab the first item because this is an array.
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +41,9 @@ class TodoListViewController: UITableViewController {
 //            itemArray = items
 //        }
         
-        loadItems()
+          print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist"))
+        
+        //loadItems()
         
     }
 
@@ -77,6 +81,19 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
     
+        //The below line of code adds a checkmark to the cell in the tableview
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        //The below code adds or removes the checkmark from the cell in the tableview, depending on if the checkmark already exists or not
+        //if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        //    tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        //} else {
+        //    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        //}
+        
+        //The below line of code removes the gray shade on the cell (which is added when it is selected) when deselecting the cell (removing your finger)
+        //tableView.deselectRow(at: indexPath, animated: true)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done //this line does the same as the below commented out if-else statement
         
 //        if itemArray[indexPath.row].done == false {
@@ -104,8 +121,11 @@ class TodoListViewController: UITableViewController {
             //print("Success!")
             //print("textField.text")
             
-            let newItem = Item()
+            //let newItem = Item()
+
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             
             self.itemArray.append(newItem)
             //could also do this:
@@ -133,29 +153,31 @@ class TodoListViewController: UITableViewController {
     //MARK - Model Manipulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder() //this encodes our data into a property list using NSCoder
+        //let encoder = PropertyListEncoder() //this encodes our data into a property list using NSCoder
         
         do {
-            let data = try encoder.encode(itemArray) //encodes data into a plist
-            try data.write(to: dataFilePath!) //write data to data file path
+            //let data = try encoder.encode(itemArray) //encodes data into a plist
+            //try data.write(to: dataFilePath!) //write data to data file path
+            try context.save() //transfers everything in the context to the permanent data storage
         } catch {
-            print("Error encoding item array, \(error)")
+            //print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) { //try? will turn the result of the Data() method into an optional. We use optional binding to unwrap that safely
-            let decoder = PropertyListDecoder() //this decodes our data from the property list using NSCoder
-            do {
-                itemArray = try decoder.decode([Item].self, from: data) //Decodes data into an array of Items. [Item] is the type of data that is being decoded
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
-            
-        }
-    }
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) { //try? will turn the result of the Data() method into an optional. We use optional binding to unwrap that safely
+//            let decoder = PropertyListDecoder() //this decodes our data from the property list using NSCoder
+//            do {
+//                itemArray = try decoder.decode([Item].self, from: data) //Decodes data into an array of Items. [Item] is the type of data that is being decoded
+//            } catch {
+//                print("Error decoding item array, \(error)")
+//            }
+//
+//        }
+//    }
     
 }
 
